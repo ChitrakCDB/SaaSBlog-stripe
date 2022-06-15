5497f54df39416912b94ad3070d679921b12e3a3
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-15 15:12:35 +0530

    add permissions to posts for active subscribers

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
     validates :title, :content, presence: true
+    scope :free, ->{where(premium: false)}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_090027) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_093118) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_090027) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "plan"
+    t.string "subscription_status", default: "incomplete"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
