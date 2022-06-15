6bbba6c2f0a2c037cfbaf6b4b5b2848534616227
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-15 14:27:17 +0530

    gem faker to generate posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+15.times do
+    Post.create(
+        title: Faker::Lorem.sentence(word_count: 3),
+        content:  Faker::Lorem.sentence,
+        premium: [true, false]. sample
+    )
+
+end    
+ 
