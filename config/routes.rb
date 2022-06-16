cf2da51a7280af232b7790ac22aa97c84e24a6be
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-16 18:12:16 +0530

    add checkout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   get "pricing", to: "static_pages#pricing"
+  post "checkout/create", to:"checkout#create", as:"checkout_create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
