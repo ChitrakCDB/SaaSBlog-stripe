934fb65fbf71792ad12ee5d6dac92c13562e494e
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-16 16:28:20 +0530

    add pricing page to the app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :posts
+  get "pricing", to: "static_pages#pricing"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
