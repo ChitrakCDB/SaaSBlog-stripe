ef42edb61204a963f35ca8d310db6ceef4e72706
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-17 11:02:49 +0530

    add select subscription plan functionality for user and purchase option for it

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,12 +3,12 @@ class CheckoutController < ApplicationController
   
     def create
       @session = Stripe::Checkout::Session.create({
-        #customer: current_user.stripe_customer_id,
+        customer: current_user.stripe_customer_id,
         success_url: posts_url,
         cancel_url: pricing_url,
         payment_method_types: ['card'],
         line_items: [
-          {price:'price_1LAuhySAytrgRr6VpDsKiIlc', quantity: 1},
+          {price: params[:price], quantity: 1},
         ],
         mode: 'subscription',
       })
