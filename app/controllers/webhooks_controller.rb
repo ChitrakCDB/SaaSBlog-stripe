06e3027c712455b9a52a0dfd65c6509a6a1afc37
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-17 17:34:44 +0530

    fix: change webhook stripe key

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -27,12 +27,12 @@ class WebhooksController < ApplicationController
         session = event.data.object
         @user = User.find_by(stripe_customer_id: session.customer)
         @user.update(subscription_status: 'active')
-      when 'customer.subscription.deleted'
+      when 'customer.subscription.updated', 'customer.subscription.deleted'
         puts "===================================================================================="
         subscription = event.data.object
         @user = User.find_by(stripe_customer_id: subscription.customer)
         @user.update(
-          subscription_status: 'incomplete',
+          subscription_status: subscription.status,
           plan: subscription.items.data[0].price.lookup_key,
         )
       end
