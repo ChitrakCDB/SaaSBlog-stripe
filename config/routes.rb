8cd39a7317c6b0f029e8af1d6f31c2d838441866
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-17 11:51:22 +0530

    add billing portal for the user to update/cancel subscription

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get "pricing", to: "static_pages#pricing"
   post "checkout/create", to:"checkout#create", as:"checkout_create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post "billing_portal/create", to:"billing_portal#create", as:"billing_portal_create"
+  
   # Defines the root path route ("/")
    root "posts#index"
 end
