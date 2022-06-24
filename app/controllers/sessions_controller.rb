6a9db410b7d5c40ce49e4a00e08b7ed88aa4365b
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-06-24 11:23:03 +0530

    feat: add omni auth to the app using googleOauth2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Users::SessionsController < Devise::SessionsController
+class SessionsController < Devise::SessionsController
     def after_sign_out_path_for(_resource_or_scope)
       new_user_session_path
     end
